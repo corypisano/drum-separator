20cd396021171d0ac0ef6134578309e4ca62afba
cory pisano <cory.pisano@gmail.com>
2019-11-19 02:11:38 -0500

    read file in upload component

--- frontend/src/components/Upload.jsx
+++ frontend/src/components/Upload.jsx
@@ -1,30 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 
-function Upload() {
-  return (
+class Upload extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      file: ''
+    };
+    
+    this.readFile = this.readFile.bind(this);
+  }
+  
+  readFile(e) {
+    // TODO check for file valid file & type
+    // flash error if invalid
+    this.setState({file: e.target.files[0]});
+  }
 
-    <Grid style={{ 'padding-bottom': 50, 'padding-top': 50 }} container spacing={1} justify="center">
-      <Grid item xs={8}>
+  render() {
+    return (
 
-        <input
-          accept="*.mp3"
-          style={{ display: 'none' }}
-          id="raised-button-file"
-          multiple
-          type="file"
-        />
-        <label htmlFor="raised-button-file">
-          <Button variant="contained" component="span" color="primary">
-            <CloudUploadIcon style={{ 'padding-right': 5 }} /> Upload
-          </Button>
-        </label>
+      <Grid style={{ 'padding-bottom': 50, 'padding-top': 50 }} container spacing={1} justify="center">
+        <Grid item xs={8}>
 
+          <input
+            accept="*.mp3"
+            style={{ display: 'none' }}
+            id="raised-button-file"
+            multiple
+            type="file"
+            onChange={(e) => {this.readFile(e)}}
+          />
+          <label htmlFor="raised-button-file">
+            <Button variant="contained" component="span" color="primary">
+              <CloudUploadIcon style={{ 'padding-right': 5 }} /> Upload
+            </Button>
+          </label>
+
+        </Grid>
       </Grid>
-    </Grid>
-  );
+    );
+  }
 }
 
 export default Upload;
